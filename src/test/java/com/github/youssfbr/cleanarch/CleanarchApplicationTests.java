9fa25ff34c5338314f0bbac53968ed48043d811d
Alisson Youssf <youssfbr@gmail.com>
2023-12-30 17:40:07 -0300

    Initial commit

---
+++ src/test/java/com/github/youssfbr/cleanarch/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
